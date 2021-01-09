474149682624859659c41d98b8ca2cc7453320df
davidbench <benshimol.d@gmail.com>
2021-01-09 00:42:07 -0500

    fixed bugs - final

--- LatencyMeter.ino
+++ LatencyMeter.ino
@@ -17,7 +17,7 @@ const int Audio_PD_pin  = 13;
 const int Audio1_pin = 2;
 const int Audio2_pin = 4;
 
-uint32_t th1_value = 1000;
+uint32_t th1_value = 3200;
 uint32_t th2_value = 1000;
 const int audio_th = 1100;
 
@@ -37,11 +37,12 @@ const unsigned long ref_period = 200000; //after result no new trigger
 int buzz=13;                   //piezo buzzer to digital pin 13  
 int senRead=0;                 //Readings from sensor to analog pin 0  
 int limit=850;                 //Threshold range of an obstacle  
-
+int step=0;
 
 unsigned long last_pixel = 0;
 unsigned long last_scale = 0;
-unsigned long micros_now = 0;
+unsigned long micros_now_reader = 0;
+unsigned long micros_now_pixel = 0;
 unsigned long result = 0;
 unsigned long result_time = 0;
 unsigned long last_video_on = 0;
@@ -55,8 +56,8 @@ NexWaveform audio = NexWaveform(0, 10, "audio");
 NexWaveform main  = NexWaveform(0,  9, "main");
 NexSlider th1 = NexSlider(0, 5, "th1");
 NexSlider th2 = NexSlider(0, 6, "th2");
-NexRadio opt_VA = NexRadio(0, 12, "opt_VA");
-NexRadio opt_AV = NexRadio(0, 13, "opt_AV");
+NexRadio opt_VA = NexRadio(0, 11, "opt_VA");
+NexRadio opt_AV = NexRadio(0, 12, "opt_AV");
 NexNumber pDelay = NexNumber(0, 8, "pDelay");
 NexNumber r1 = NexNumber(0, 2, "r1");
 NexNumber r2 = NexNumber(0, 3, "r2");
@@ -91,18 +92,14 @@ NexTouch *nex_listen_list[] = {
 void Pixel( void * pvParameters ){
   while (true) {
     
-    micros_now = micros();
-    int measured_pixel = micros_now - last_pixel;
+    micros_now_pixel = micros();
+    int measured_pixel = micros_now_pixel - last_pixel;
+
     if (measured_pixel > pixel_delay) {
-      
       if (last_pixel<result_time)
         update_result(result);
 
-      bool pixel_video_on = video_on || (last_pixel<last_video_on);
-      bool pixel_audio_on = audio_on || (last_pixel<last_audio_on);
-        
-      last_pixel = micros_now;
-      
+       
       video.addValue(0,
         map(PD_val_pixel,       0, BIT12MAX, 0+2, CHART_MAX-2));
       video.addValue(1,
@@ -116,23 +113,73 @@ void Pixel( void * pvParameters ){
       audio.addValue(3,
         map(th2_value,          0, BIT12MAX, 0+2, CHART_MAX-2));
 
+      bool pixel_video_on = video_on || (last_pixel<last_video_on);
+      bool pixel_audio_on = audio_on || (last_pixel<last_audio_on);
+        
+      last_pixel = micros_now_pixel;
+
       alternator = !alternator;
-      main.addValue( 0, 
+      int rc = (10 * (micros_now_pixel - trigger_time)) / timeout; //result clock
+      main.addValue(0, 
+          20 +
+          alternator *
+          (
+              18 * pixel_video_on +
+              !pixel_video_on *
+              (
+                rc * (!pixel_audio_on &&  triggered) +
+               -rc * ( pixel_audio_on &&  triggered)
+              )
+          ) +
+          !alternator *
+          (
+              -18 * pixel_audio_on +
+              !pixel_audio_on *
+              (
+               -rc * (!pixel_video_on &&  triggered) +
+                rc * ( pixel_video_on &&  triggered)
+              )
+             
+             
+          )
+         );
+      
+      /*
+          0 +
+         (alternator || !triggered) *
+         (
+            15 * pixel_audio_on +
+            22 * pixel_video_on
+         ) +
+         (!alternator && triggered) *
+         (
+            (8 * (micros_now_pixel - trigger_time)) / timeout
+         )
+         );
+         
+        */ 
+       /*   
+        
           7 +
          -1 * triggered*(!pixel_video_on)*(!pixel_audio_on)*(2+4*alternator) +
          10 * pixel_audio_on +
          20 * pixel_video_on
-        );
-
+        );*/
       video_pixel_just_written = true;
       audio_pixel_just_written = true;
-        
-      measured.setValue(measured_pixel);    
+
+      if (step>(5000000.0/pixel_delay))
+      {
+        measured.setValue(measured_pixel);
+        step=0;
+      }
+      step++;
+      
     }
     //else update for maximum in boolean
     
     nexLoop(nex_listen_list);
-
+    
 //Serial.printf ("$%d %d %d %d %d %d;", PD_val, video_on, Audio_PD_val, Audio1_val, Audio2_val, audio_on);
         //Serial.printf ("%d %d %d %d %d %d", PD_val, video_on, Audio_PD_val, Audio1_val, Audio2_val, audio_on);
         //Serial.println(" ");
@@ -144,29 +191,29 @@ delay(1);
 void Reader( void * pvParameters ){
      while(true){
       
-        micros_now = micros();
+        micros_now_reader = micros();
         PD_val=analogRead(PhotoDiode_pin);  //variable to store values from the photodiode  
-        micros_now = (micros() + micros_now)/2;
+        micros_now_reader = (micros() + micros_now_reader)/2;
 
-        refractory = refractory && ((micros_now - result_time)<ref_period);
+        refractory = refractory && ((micros_now_reader - result_time)<ref_period);
         
         video_on = PD_val>th1_value;
         if (video_on)
-          last_video_on = micros_now;
+          last_video_on = micros_now_reader;
         
         if (mode_VA) {
           if (video_on && !triggered && !refractory) {
             triggered = true;
-            trigger_time = micros_now;
+            trigger_time = micros_now_reader;
           }
           else {
-            triggered = triggered && ((micros_now - trigger_time)<timeout);
+            triggered = triggered && ((micros_now_reader - trigger_time)<timeout);
           }
         }
         else {
           if (video_on) {
             if (triggered) {
-              result = micros_now-trigger_time;
+              result = micros_now_reader-trigger_time;
               result_time = micros();
               triggered = false;
               refractory = true;
@@ -178,31 +225,31 @@ void Reader( void * pvParameters ){
         }
         
         
-        micros_now = micros();
+        micros_now_reader = micros();
         Audio_PD_val=analogRead(Audio_PD_pin);
         Audio1_val=analogRead(Audio1_pin) * 2; /////
         Audio2_val=analogRead(Audio2_pin);
-        micros_now = (micros() + micros_now)/2;
+        micros_now_reader = (micros() + micros_now_reader)/2;
 
-        refractory = refractory && ((micros_now - result_time)<ref_period);
+        refractory = refractory && ((micros_now_reader - result_time)<ref_period);
         
         audio_on = (Audio1_val<audio_th) || (Audio2_val<audio_th) || (Audio_PD_val<th2_value);
         if (audio_on)
-          last_audio_on = micros_now;
+          last_audio_on = micros_now_reader;
         
         if (!mode_VA) {
           if (audio_on && !triggered && !refractory) {
             triggered = true;
-            trigger_time = micros_now;
+            trigger_time = micros_now_reader;
           }
           else {
-            triggered = triggered && ((micros_now - trigger_time)<timeout);
+            triggered = triggered && ((micros_now_reader - trigger_time)<timeout);
           }
         }
         else {
           if (audio_on) {
             if (triggered) {
-              result = micros_now-trigger_time;
+              result = micros_now_reader-trigger_time;
               result_time = micros();
               triggered = false;
               refractory = true;
@@ -213,10 +260,10 @@ void Reader( void * pvParameters ){
           }
         }
 
-    PD_val_pixel        =    0 * video_pixel_just_written + !video_pixel_just_written * max(PD_val_pixel, PD_val);
-    Audio_PD_val_pixel  = 4095 * audio_pixel_just_written + !video_pixel_just_written * min(Audio_PD_val_pixel, Audio_PD_val);
-    Audio1_val_pixel    = 4095 * audio_pixel_just_written + !video_pixel_just_written * min(Audio1_val_pixel, Audio1_val);
-    Audio2_val_pixel    = 4095 * audio_pixel_just_written + !video_pixel_just_written * min(Audio2_val_pixel, Audio2_val);
+    PD_val_pixel        = PD_val       * video_pixel_just_written + !video_pixel_just_written * max(PD_val_pixel, PD_val);
+    Audio_PD_val_pixel  = Audio_PD_val * audio_pixel_just_written + !audio_pixel_just_written * min(Audio_PD_val_pixel, Audio_PD_val);
+    Audio1_val_pixel    = Audio1_val   * audio_pixel_just_written + !audio_pixel_just_written * min(Audio1_val_pixel, Audio1_val);
+    Audio2_val_pixel    = Audio2_val   * audio_pixel_just_written + !audio_pixel_just_written * min(Audio2_val_pixel, Audio2_val);
     video_pixel_just_written = false;
     audio_pixel_just_written = false;
 
